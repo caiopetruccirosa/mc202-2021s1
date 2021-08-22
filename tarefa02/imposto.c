5fdb27f1c01e8f592c66f4e0ccd1767cb61f4139
Caio Petrucci Rosa <caiopetruccirosa@gmail.com>
2021-08-21 23:58:15 -0300

    Adiciona inicio da tarefa02

---
+++ tarefa02/imposto.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    /*
+    Até 1.499,15	Isento
+    De 1.499,15 até 2.246,75	7,5
+    De 2.246,75 até 2.995,70	15,0
+    De 2.995,70 até 3.743,19	22,5
+    Acima de 3.743,19	27,5
+    */
+    
+    int n;
+
+    scanf("%d", &n);
+
+    return 0;
+}
