4a33b36bda1c3039c3add7785b3964d394129f7f
Caio Petrucci Rosa <caiopetruccirosa@gmail.com>
2021-08-28 20:23:36 -0300

    Adiciona comentários e outras melhorias na tarefa03

--- tarefa03/classificar.c
+++ tarefa03/classificar.c
@@ -1,46 +1,65 @@
 #include <stdio.h>
 #include <string.h>
 
+/*
+Função que lê uma quantidade 'n' de nomes de até 29 caracteres da entrada padrão.
+Conforme a leitura, a função popula um vetor de strings 'nomes' com os nomes lidos.
+*/
 void ler_nomes(char nomes[100][30], int n) {
     for (int i = 0; i < n; i++) {
         scanf("%s", nomes[i]);
     }
 }
 
+/*
+Função que imprime uma quantidade 'n' de nomes de até 29 caracteres na saída padrão.
+Os nomes devem ser passados em um vetor de strings 'nomes' de tamanho 'n'.
+*/
 void imprimir_nomes(char nomes[100][30], int n) {
     for (int i = 0; i < n; i++) {
         printf("%s\n", nomes[i]);
     }
 }
 
+/*
+Função que, dado um nome de até 29 caracteres, encontra e retorna a posição do separador
+'_' dentro da string.
+*/
 int posicao_separador(char nome[30]) {
-    int i = 0;
-
-    while (1 == 1) {
-        if (nome[i] == '\0') {
-            return -1;
-        } else if (nome[i] == '_') {
+    for (int i = 0; i < 30 && nome[i] != '\0'; i++) {
+        if (nome[i] == '_') {
             return i;
         }
-
-        i++;
     }
 
     return -1;
 }
 
-int comparar_parentes(char nome1[30], char nome2[30]) {
-    int i = posicao_separador(nome1);
-    int j = posicao_separador(nome2);
+/*
+Função que compara dois nomes, lexicograficamente, priorizando na comparação a lexicografia
+dos sobrenomes e então, se forem iguais, a lexicografia do nome completo.
+A função retorna:
+- -1 se nome1 < nome2;
+- 0 se nome1 == nome2;
+- 1 se nome1 > nome2.
+*/
+int comparar_sobrenome(char nome1[30], char nome2[30]) {
+    int separador_nome1 = posicao_separador(nome1);
+    int separador_nome2 = posicao_separador(nome2);
 
-    int ret = strcmp(&nome1[i], &nome2[j]);
-    if (ret == 0) {
-        ret = strcmp(nome1, nome2);
+    int comparacao = strcmp(&nome1[separador_nome1], &nome2[separador_nome2]);
+    if (comparacao == 0) {
+        comparacao = strcmp(nome1, nome2);
     }
 
-    return ret;
+    return comparacao;
 }
 
+/*
+Função que, dados dois nomes de até 29 caracteres, retorna um valor booleano (0 para 
+falso e 1 para verdadeiro) que indica se os nomes representam individuos parentes, ou 
+seja, com sobrenomes iguais.
+*/
 int sao_parentes(char nome1[30], char nome2[30]) {
     int i = posicao_separador(nome1);
     int j = posicao_separador(nome2);
@@ -52,6 +71,11 @@ int sao_parentes(char nome1[30], char nome2[30]) {
     return 1;
 }
 
+/*
+Função que, dados dois nomes de até 29 caracteres, retorna um valor booleano (0 para 
+falso e 1 para verdadeiro) que indica se os nomes são homonimos, a partir da comparação
+apenas dos primeiros nomes de cada nome.
+*/
 int sao_homonimos(char nome1[30], char nome2[30]) {
     char primeiro_nome1[30];
     strcpy(primeiro_nome1, nome1);
@@ -68,73 +92,89 @@ int sao_homonimos(char nome1[30], char nome2[30]) {
     return 1;
 }
 
-void ordenar_homonimos(char v[100][30], int n) {
-    for (int i = 0; i < n; i++) {
-        int menor = i;
+/*
+Função que faz o particionamento de um vetor de nomes em duas partições baseadas em 
+um  pivô escolhido, onde uma partição contem nomes "menores" do que o pivô e a outra 
+contem valor "maiores", baseado em uma 'opcao' de ordenação que resulta em regras
+diferentes de ordem.
+*/
+int particionamento(char v[100][30], int inicio, int fim, char opcao[10]) {    
+    int posicao_pivo = fim;
 
-        for (int j = i; j < n; j++) {
-            if (strcmp(v[j], v[menor]) < 0) {
-                menor = j;
-            }
-        }
-
-        if (menor != i) {
+    int i = inicio;
+    for (int j = inicio; j < fim; j ++) {
+        if ((strcmp(opcao, "parentes") == 0 && comparar_sobrenome(v[j], v[posicao_pivo]) < 0) || 
+            (strcmp(opcao, "homonimos") == 0 && strcmp(v[j], v[posicao_pivo]) < 0)) {
+            
             char aux[30];
             strcpy(aux, v[i]);
-            strcpy(v[i], v[menor]);
-            strcpy(v[menor], aux);
+            strcpy(v[i], v[j]);
+            strcpy(v[j], aux);
+
+            i++;
         }
+    }
+
+    // Coloca o pivô no lugar dele
+    if (i != posicao_pivo) {
+        char aux[30];
+        strcpy(aux, v[i]);
+        strcpy(v[i], v[posicao_pivo]);
+        strcpy(v[posicao_pivo], aux);
+
+        posicao_pivo = i;
+    }
+
+    return posicao_pivo;
+}
+
+/*
+Função que ordena um vetor de strings, de até 29 caracteres, utilizando o algoritmo
+recursivo quick_sort, baseado em uma 'opcao' de ordenação, que pode ser "homonimos" 
+ou "parentes". 
+*/
+void quick_sort(char v[100][30], int inicio, int fim, char opcao[10]) {
+    if (inicio < fim) {
+        int posicao_pivo = particionamento(v, inicio, fim, opcao);
+
+        quick_sort(v, inicio, posicao_pivo-1, opcao);
+        quick_sort(v, posicao_pivo+1, fim, opcao);
     }
 
     return;
 }
 
-void ordenar_parentes(char v[100][30], int n) {
-    for (int i = 0; i < n; i++) {
-        int menor = i;
 
-        for (int j = i; j < n; j++) {
-            if (comparar_parentes(v[j], v[menor]) < 0) {
-                menor = j;
-            }
-        }
-
-        if (menor != i) {
-            char aux[30];
-            strcpy(aux, v[i]);
-            strcpy(v[i], v[menor]);
-            strcpy(v[menor], aux);
-        }
-    }
+/*
+Função que ordena um vetor de nomes (de até 29 caracteres) de tamanho 'n'.
+Para a ordenação, é necessário passar uma 'opcao' de ordenação, que pode ser "homonimos" 
+ou "parentes" onde cada uma resulta em regras de ordenação diferentes.
+*/
+void ordenar_nomes(char nomes[100][30], int n, char opcao[10]) {
+    quick_sort(nomes, 0, n-1, opcao);
 
     return;
 }
 
-void filtrar_homonimos(char nomes[100][30], int n, char homonimos[100][30], int *m) {
+/*
+Função que filtra os nomes em um vetor 'nomes' baseado em uma 'opcao', que pode conter os
+valores "homonimos" ou "parentes", e armazena os valores filtrados em um vetor 'filtrados'
+que terá tamanho 'm'.
+*/
+void filtrar_nomes(char nomes[100][30], int n, char filtrados[100][30], int *m, char opcao[10]) {
     *m = 0;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            if (i != j && sao_homonimos(nomes[i], nomes[j]) == 1) {
-                strcpy(homonimos[*m], nomes[i]);
-                (*m)++;
-                
-                break;
-            }
-        }
-    }
+            if (i != j) {
+                int homonimos = sao_homonimos(nomes[i], nomes[j]);
+                int parentes = sao_parentes(nomes[i], nomes[j]);
 
-    return;
-}
+                if ((strcmp(opcao, "homonimos") == 0 && homonimos) || (strcmp(opcao, "parentes") == 0 && parentes)) {
+                    strcpy(filtrados[*m], nomes[i]);
+                    (*m)++;
 
-void filtrar_parentes(char nomes[100][30], int n, char parentes[100][30], int *m) {
-    *m = 0;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (i != j && sao_parentes(nomes[i], nomes[j]) == 1) {
-                strcpy(parentes[*m], nomes[i]);
-                (*m)++;
-                
-                break;
+                    break;
+                }
             }
         }
     }
@@ -153,16 +193,11 @@ int main() {
     ler_nomes(nomes, n);
 
     int m;
-    char resultado[100][30];
-    if (strcmp(opcao, "homonimos") == 0) {
-        filtrar_homonimos(nomes, n, resultado, &m);
-        ordenar_homonimos(resultado, m);
-    } else if (strcmp(opcao, "parentes") == 0) {
-        filtrar_parentes(nomes, n, resultado, &m);
-        ordenar_parentes(resultado, m);
-    }
+    char classificados[100][30];
+    filtrar_nomes(nomes, n, classificados, &m, opcao);
+    ordenar_nomes(classificados, m, opcao);
 
-    imprimir_nomes(resultado, m);
+    imprimir_nomes(classificados, m);
 
     return 0;
 }
